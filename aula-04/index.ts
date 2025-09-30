c9c4316770119dc3206bdce2bbf4d851f6f2860d
Isis Jardim <isinha.jardim@gmail.com>
2025-09-29 21:14:01 -0300

    feat: adicionar estrutura de estoque com itens e quantidades

---
+++ aula-04/index.ts
@@ -0,0 +1,12 @@
+type ItemEstoque = {
+    produto: string;
+    quantidade: number;
+    minimo: number;
+};
+
+let estoque: ItemEstoque[] = [
+    { produto: "Arroz", quantidade: 50, minimo: 20 },
+    { produto: "Feijão", quantidade: 15, minimo: 20 },
+    { produto: "Macarrão", quantidade: 5, minimo: 10 },
+    { produto: "Açúcar", quantidade: 30, minimo: 15 }
+];
